97e8fbd177e5bf95233939af2dd8426196ae059c
nmelgar <nmelgar16013@gmail.com>
2025-03-08 18:59:13 -0600

    Add to check if symbol exist. Display basic ticker info. Chart closing pirce and 20-day moving average. Chart for daily returns.

--- try.py
+++
@@ -1,7 +0,0 @@
-#%%
-import pandas as pd
-
-df = pd.read_json('https://raw.githubusercontent.com/nmelgar/tickers-web-scrap-python/refs/heads/main/stock_tickers_data.json')
-
-print(df.to_string()) 
-# %%
